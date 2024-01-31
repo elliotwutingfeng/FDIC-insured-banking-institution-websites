ab204053ac40a36d14c2cb5c08a08c57e26e9ef9
Wu Tingfeng <wutingfeng@outlook.com>
2024-01-31 09:50:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# FDIC-insured-banking-institution-websites
+
+![Python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![GitHub Actions](https://img.shields.io/badge/GitHub_Actions-2088FF?style=for-the-badge&logo=github-actions&logoColor=white)
+
+[![LICENSE](https://img.shields.io/badge/LICENSE-BSD--3--CLAUSE-GREEN?style=for-the-badge)](LICENSE)
+[![scraper](https://img.shields.io/github/actions/workflow/status/elliotwutingfeng/FDIC-insured-banking-institution-websites/scraper.yml?branch=main&label=SCRAPER&style=for-the-badge)](https://github.com/elliotwutingfeng/FDIC-insured-banking-institution-websites/actions/workflows/scraper.yml)
+![Total Allowlist URLs](https://tokei-rs.onrender.com/b1/github/elliotwutingfeng/FDIC-insured-banking-institution-websites?label=Total%20Allowlist%20URLS&style=for-the-badge)
+
+Machine-readable `.txt` allowlist of websites belonging to banking institutions insured by the [United States FDIC (Federal Deposit Insurance Corporation)](https://fdic.gov), updated once a day.
+
+Data is sourced from the public domain [FDIC BankFind API](https://banks.data.fdic.gov).
+
+**Disclaimer:** _This project is not sponsored, endorsed, or otherwise affiliated with the United States Government._
+
+## Allowlist download
+
+| File | Download |
+|:-:|:-:|
+| urls.txt | [:floppy_disk:](urls.txt?raw=true) |
+| urls-pihole.txt | [:floppy_disk:](urls-pihole.txt?raw=true) |
+| ips.txt | [:floppy_disk:](ips.txt?raw=true) |
+
+## Requirements
+
+- Python 3.12+
+
+## Setup instructions
+
+`git clone` and `cd` into the project directory, then run the following
+
+```bash
+python3 -m venv venv
+venv/bin/python3 -m pip install --upgrade pip
+venv/bin/python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+venv/bin/python3 scraper.py
+```
+
+## Libraries/Frameworks used
+
+- [tldextract](https://github.com/john-kurkowski/tldextract)
+
+&nbsp;
+
+<sup>These files are provided "AS IS", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, arising from, out of or in connection with the files or the use of the files.</sup>
+
+<sub>Any and all trademarks are the property of their respective owners.</sub>
---
+++ scraper.py
@@ -0,0 +1,154 @@
+"""Extract United States FDIC-insured banking institution websites from the BankFind API
+and write them to a .txt allowlist
+"""
+
+import ipaddress
+import logging
+import math
+import re
+import socket
+from datetime import datetime
+
+import requests
+import tldextract
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def current_datetime_str() -> str:
+    """Current time's datetime string in UTC
+
+    Returns:
+        str: Timestamp in strftime format "%d_%b_%Y_%H_%M_%S-UTC".
+    """
+    return datetime.utcnow().strftime("%d_%b_%Y_%H_%M_%S-UTC")
+
+
+def clean_url(url: str) -> str:
+    """Remove zero width spaces, leading/trailing whitespaces, trailing slashes,
+    and URL prefixes from a URL
+
+    Args:
+        url (str): URL.
+
+    Returns:
+        str: URL without zero width spaces, leading/trailing whitespaces, trailing slashes,
+    and URL prefixes.
+    """
+    removed_zero_width_spaces = re.sub(r"[\u200B-\u200D\uFEFF]", "", url)
+    removed_leading_and_trailing_whitespaces = removed_zero_width_spaces.strip()
+    removed_trailing_slashes = removed_leading_and_trailing_whitespaces.rstrip("/")
+    removed_https = re.sub(r"^[Hh][Tt][Tt][Pp][Ss]:\/\/", "", removed_trailing_slashes)
+    removed_http = re.sub(r"^[Hh][Tt][Tt][Pp]:\/\/", "", removed_https)
+
+    return removed_http
+
+
+def extract_urls() -> set[str]:
+    """Extract United States FDIC-insured banking institution websites from the BankFind API
+
+    Returns:
+        set[str]: Unique United States FDIC-insured banking institution URLs.
+    """
+    url_column_names: list[str] = ["TE%sN528" % str(i + 1).zfill(2) for i in range(10)]
+
+    try:
+        # Get total entries
+        res: requests.Response = requests.get(
+            "https://banks.data.fdic.gov/api/institutions", timeout=30
+        )
+        res.raise_for_status()
+        max_entries_per_call: int = 10000
+        total: int = res.json().get("meta", 0).get("total", 0)
+        num_pages: int = int(math.ceil(total / max_entries_per_call))
+
+        # Get active institutions
+        institutions = []
+        for page in range(num_pages):
+            res = requests.get(
+                "https://banks.data.fdic.gov/api/institutions?filters=ACTIVE:1&fields=NAME,"
+                f"{','.join(url_column_names)}&limit={max_entries_per_call}&offset={page*max_entries_per_call}",
+                timeout=60,
+            )
+            if res.status_code != 200:
+                continue
+            institutions.extend(
+                [y for x in res.json().get("data", []) if (y := x.get("data", None))]
+            )
+
+        institutions_with_urls = []
+        for institution in institutions:
+            institution_name: str = institution.get("NAME", "").strip()
+            institution_urls = sorted(
+                set(institution.get(col, "").strip() for col in url_column_names)
+                - set([""])
+            )
+            if institution_name and institution_urls:
+                institutions_with_urls.append((institution_name, institution_urls))
+
+        urls: set[str] = set()
+        for _, institution_urls in institutions_with_urls:
+            for url in institution_urls:
+                urls.add(clean_url(url))
+        return urls
+    except Exception as error:
+        logger.error(error)
+        return set()
+
+
+if __name__ == "__main__":
+    urls: set[str] = extract_urls()
+    ips: set[str] = set()
+    non_ips: set[str] = set()
+    fqdns: set[str] = set()
+
+    if not urls:
+        raise ValueError("Failed to scrape URLs")
+    for url in urls:
+        res = tldextract.extract(url)
+        domain, fqdn = res.domain, res.fqdn
+        if domain and not fqdn:
+            # Possible IPv4 Address
+            try:
+                socket.inet_pton(socket.AF_INET, domain)
+                ips.add(domain)
+            except socket.error:
+                # Is invalid URL and invalid IP -> skip
+                pass
+        elif fqdn:
+            non_ips.add(url)
+            fqdns.add(fqdn.lower())
+
+    if not non_ips and not ips:
+        logger.error("No content available for allowlists.")
+    else:
+        non_ips_timestamp: str = current_datetime_str()
+        non_ips_filename = "urls.txt"
+        with open(non_ips_filename, "w") as f:
+            f.writelines("\n".join(sorted(non_ips)))
+            logger.info(
+                "%d non-IPs written to %s at %s",
+                len(non_ips),
+                non_ips_filename,
+                non_ips_timestamp,
+            )
+
+        ips_timestamp: str = current_datetime_str()
+        ips_filename = "ips.txt"
+        with open(ips_filename, "w") as f:
+            f.writelines("\n".join(sorted(ips, key=ipaddress.IPv4Address)))
+            logger.info(
+                "%d IPs written to %s at %s", len(ips), ips_filename, ips_timestamp
+            )
+
+        fqdns_timestamp: str = current_datetime_str()
+        fqdns_filename = "urls-pihole.txt"
+        with open(fqdns_filename, "w") as f:
+            f.writelines("\n".join(sorted(fqdns)))
+            logger.info(
+                "%d FQDNs written to %s at %s",
+                len(fqdns),
+                fqdns_filename,
+                fqdns_timestamp,
+            )
